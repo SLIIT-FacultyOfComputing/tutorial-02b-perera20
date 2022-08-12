6b4758ba072bb87e58e54fd07c6377b77bdd121d
perera20 <110513552+perera20@users.noreply.github.com>
2022-08-12 11:05:10 +0000

    c language codes to c++ languages

--- tute04.cpp
+++ tute04.cpp
@@ -12,11 +12,22 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
+#include <string>
+int longFactorial(int no){
+  if (no > 0) 
+       return no + longFactorial(no - 1);
+  else 
+       return 0;
+  }
 
-long Factorial(int no);
-long nCr(int n, int r);
+int longnCr(int n, int r){
+  int nCr;
+  nCr = r * (n-r);
+  return 0;
+}
 
 int main() {
+  
   int n, r;
   std::cout << "Enter a value for n ";
   std::cin >> n;
@@ -25,5 +36,13 @@ int main() {
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
+
+  int num = longFactorial(n);
+  std::cout << n ;
   return 0;
+
+  int nCr = longnCr(n,r);
+  std::cout << nCr ;
+  return 0;
+  
 }
